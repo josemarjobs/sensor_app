55f8ee43f5971dcf65cff840c939ea3ecc2dfb0c
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-12-29 15:24:10 -0800

    Build a message sender

---
+++ notes.sh
@@ -0,0 +1,13 @@
+# start rabbitmq
+rabbitmq-server
+
+rabbitmq-ctl 
+  status
+  list_queues
+  cluster_status
+
+# list plugins
+rabbitmq-plugins
+
+# enable/disable plugins
+rabbitmq-plugins disable|enable plugin-name
