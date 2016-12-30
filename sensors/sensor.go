cfaa14315dc92f5c87d0529b19457a0abc74efde
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-12-29 16:58:18 -0800

    Create conn, ch and q

---
+++ sensors/sensor.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"bytes"
+
+	"github.com/josemarjobs/sensors_app/dto"
+)
+
+var url = "amqp://guest:guest@localhost:5672"
+
+var name = flag.String("name", "sensor", "name of the sensor")
+var freq = flag.Uint("freq", 5, "update frequency in cycles/sec")
+var max = flag.Float64("max", 5., "maximum value for generated reading")
+var min = flag.Float64("min", 1., "minimum value for generated reading")
+var stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement.")
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+var value = r.Float64()*(*max-*min) + *min
+var nom = (*max-*min)/2 + *min
+
+func main() {
+	flag.Parse()
+
+	dur, _ := time.ParseDuration(strconv.Itoa(1000/int(*freq)) + "ms")
+
+	signal := time.Tick(dur)
+
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+
+	for range signal {
+		calcValue()
+		reading := dto.SensorMessage{
+			Name:      *name,
+			Value:     value,
+			Timestamp: time.Now(),
+		}
+
+		buf.Reset()
+		enc.Encode(reading)
+
+		log.Printf("Reading sent. Value: %v\n", value)
+	}
+}
+
+func calcValue() {
+	var maxStep, minStep float64
+
+	if value < nom {
+		maxStep = *stepSize
+		minStep = -1 * *stepSize * (value - *min) / (nom - *min)
+	} else {
+		maxStep = *stepSize * (*max - value) / (*max - nom)
+		minStep = -1 * *stepSize
+	}
+
+	value += r.Float64()*(maxStep-minStep) + minStep
+}
